b4e2f234bc4c2de12aa779ddb751e2ec21fd2abc
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-10-07 15:01:10 +0900

    problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,28 @@
 function problem1(pobi, crong) {
   var answer;
+
+  if (pobi[1] !== pobi[0]+1 || crong[1] !== crong[0]+1) {
+    return -1;
+  }
+
+  // pobi
+  let pobi_left = Math.max(String(pobi[0]).split('').map(Number).reduce((a,b)=>a*b),
+      String(pobi[0]).split('').map(Number).reduce((a,b)=>a+b));
+  let pobi_right = Math.max(String(pobi[1]).split('').map(Number).reduce((a,b)=>a*b),
+      String(pobi[1]).split('').map(Number).reduce((a,b)=>a+b));
+  let pobi_answer = Math.max(pobi_left, pobi_right);
+
+  // crong
+  let crong_left = Math.max(String(crong[0]).split('').map(Number).reduce((a,b)=>a*b),
+      String(crong[0]).split('').map(Number).reduce((a,b)=>a+b));
+  let crong_right = Math.max(String(crong[1]).split('').map(Number).reduce((a,b)=>a*b),
+      String(crong[1]).split('').map(Number).reduce((a,b)=>a+b));
+  let crong_answer = Math.max(crong_left, crong_right);
+
+  if (pobi_answer > crong_answer) answer = 1;
+  else if (pobi_answer < crong_answer) answer = 2;
+  else if (pobi_answer === crong_answer) answer = 0;
+
   return answer;
 }
 
